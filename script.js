3059ab911af443cddc34f35148d4d80142b30ae3
c08012613 <158455628+c08012613@users.noreply.github.com>
2024-02-07 21:32:04 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.body.onload = addAnimation;
+
+function addAnimation() {
+    // This function is left empty for simplicity
+    // You can add more complex animations or JS effects here
+}
